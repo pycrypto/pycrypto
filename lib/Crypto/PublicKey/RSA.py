d0c3acc93e4fbebe541f1f27f7950980e1cac2bc
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2011-06-24 16:25:22 -0500

    Add a SSH format in RSA object to export rsa-ssh keys

--- lib/Crypto/PublicKey/RSA.py
+++ lib/Crypto/PublicKey/RSA.py
@@ -26,7 +26,7 @@
 
 __revision__ = "$Id$"
 
-__all__ = ['generate', 'construct', 'error', 'importKey' ]
+__all__ = ['generate', 'construct', 'error', 'importKey']
 
 from Crypto.Util.python_compat import *
 from Crypto.Util.number import getRandomRange
@@ -36,11 +36,13 @@ from Crypto import Random
 
 from Crypto.Util.asn1 import DerObject, DerSequence
 import binascii
+import base64
 
 try:
     from Crypto.PublicKey import _fastmath
 except ImportError:
     _fastmath = None
+
 
 class _RSAobj(pubkey.pubkey):
     keydata = ['n', 'e', 'd', 'p', 'q', 'u']
@@ -58,7 +60,8 @@ class _RSAobj(pubkey.pubkey):
             # RSA key parameters directly from this object.
             return getattr(self.key, attrname)
         else:
-            raise AttributeError("%s object has no %r attribute" % (self.__class__.__name__, attrname,))
+            raise AttributeError("%s object has no %r attribute" % (
+                self.__class__.__name__, attrname,))
 
     def _encrypt(self, c, K):
         return (self.key._encrypt(c),)
@@ -66,13 +69,14 @@ class _RSAobj(pubkey.pubkey):
     def _decrypt(self, c):
         #(ciphertext,) = c
         (ciphertext,) = c[:1]  # HACK - We should use the previous line
-                               # instead, but this is more compatible and we're
-                               # going to replace the Crypto.PublicKey API soon
-                               # anyway.
+            # instead, but this is more compatible and we're
+            # going to replace the Crypto.PublicKey API soon
+            # anyway.
 
         # Blinded RSA decryption (to prevent timing attacks):
-        # Step 1: Generate random secret blinding factor r, such that 0 < r < n-1
-        r = getRandomRange(1, self.key.n-1, randfunc=self._randfunc)
+        # Step 1: Generate random secret blinding factor r,
+        #         such that 0 < r < n-1
+        r = getRandomRange(1, self.key.n - 1, randfunc=self._randfunc)
         # Step 2: Compute c' = c * r**e mod n
         cp = self.key._blind(ciphertext, r)
         # Step 3: Compute m' = c'**d mod n       (ordinary RSA decryption)
@@ -92,8 +96,8 @@ class _RSAobj(pubkey.pubkey):
     def _verify(self, m, sig):
         #(s,) = sig
         (s,) = sig[:1]  # HACK - We should use the previous line instead, but
-                        # this is more compatible and we're going to replace
-                        # the Crypto.PublicKey API soon anyway.
+            # this is more compatible and we're going to replace
+            # the Crypto.PublicKey API soon anyway.
         return self.key._verify(m, s)
 
     def has_private(self):
@@ -128,7 +132,7 @@ class _RSAobj(pubkey.pubkey):
             self.implementation = RSAImplementation()
         t = []
         for k in self.keydata:
-            if not d.has_key(k):
+            if k not in d:
                 break
             t.append(d[k])
         self.key = self.implementation._math.rsa_construct(*tuple(t))
@@ -137,45 +141,75 @@ class _RSAobj(pubkey.pubkey):
         attrs = []
         for k in self.keydata:
             if k == 'n':
-                attrs.append("n(%d)" % (self.size()+1,))
+                attrs.append("n(%d)" % (self.size() + 1,))
             elif hasattr(self.key, k):
                 attrs.append(k)
         if self.has_private():
             attrs.append("private")
-        return "<%s @0x%x %s>" % (self.__class__.__name__, id(self), ",".join(attrs))
+        return "<%s @0x%x %s>" % (
+            self.__class__.__name__, id(self), ",".join(attrs))
 
     def exportKey(self, format='PEM'):
-	"""Export the RSA key. A string is returned
-	with the encoded public or the private half
-	under the selected format.
+        """Export the RSA key. A string is returned
+        with the encoded public or the private half
+        under the selected format.
 
-	format:		'DER' (PKCS#1) or 'PEM' (RFC1421)
-	"""
-	der = DerSequence()
-	if self.has_private():
-		keyType = "RSA PRIVATE"
-		der[:] = [ 0, self.n, self.e, self.d, self.p, self.q,
-			   self.d % (self.p-1), self.d % (self.q-1),
-			   self.u ]
-	else:
-		keyType = "PUBLIC"
-		der.append('\x30\x0D\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x01\x01\x05\x00')
-		bitmap = DerObject('BIT STRING')
-		derPK = DerSequence()
-		derPK[:] = [ self.n, self.e ]
-		bitmap.payload = '\x00' + derPK.encode()
-		der.append(bitmap.encode())
-	if format=='DER':
-		return der.encode()
-	if format=='PEM':
-		pem = "-----BEGIN %s KEY-----\n" % keyType
-		binaryKey = der.encode()
-		# Each BASE64 line can take up to 64 characters (=48 bytes of data)
-		chunks = [ binascii.b2a_base64(binaryKey[i:i+48]) for i in range(0, len(binaryKey), 48) ]
-		pem += ''.join(chunks)
-		pem += "-----END %s KEY-----" % keyType
-		return pem
-	return ValueError("")
+        format:		'DER' (PKCS#1) or 'PEM' (RFC1421)
+        """
+        der = DerSequence()
+        if self.has_private():
+            keyType = "RSA PRIVATE"
+            der[:] = [0, self.n, self.e, self.d, self.p, self.q,
+                      self.d % (self.p - 1), self.d % (self.q - 1),
+                      self.u]
+        else:
+            keyType = "PUBLIC"
+            der.append(
+                '\x30\x0D\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x01\x01\x05\x00')
+            bitmap = DerObject('BIT STRING')
+            derPK = DerSequence()
+            derPK[:] = [self.n, self.e]
+            bitmap.payload = '\x00' + derPK.encode()
+            der.append(bitmap.encode())
+        if format == 'DER':
+            return der.encode()
+        if format == 'PEM':
+            pem = "-----BEGIN %s KEY-----\n" % keyType
+            binaryKey = der.encode()
+            # Each BASE64 line can take up to 64 characters (=48 bytes of data)
+            chunks = []
+            for i in range(0, len(binaryKey), 48):
+                chunks.append(binascii.b2a_base64(binaryKey[i:i + 48]))
+            pem += ''.join(chunks)
+            pem += "-----END %s KEY-----" % keyType
+            return pem
+        if format == 'SSH':
+            # Create public key.
+            ssh_rsa = '00000007' + base64.b16encode('ssh_rsa')
+
+            # Exponent.
+            exponent = '%x' % (self.key.e, )
+            if len(exponent) % 2:
+                exponent = '0' + exponent
+
+            ssh_rsa += '%08x' % (len(exponent) / 2, )
+            ssh_rsa += exponent
+
+            modulus = '%x' % (self.key.n, )
+            if len(modulus) % 2:
+                modulus = '0' + modulus
+
+            if modulus[0] in '89abcdef':
+                modulus = '00' + modulus
+
+            ssh_rsa += '%08x' % (len(modulus) / 2, )
+            ssh_rsa += modulus
+
+            return 'ssh-rsa %s' % (base64.b64encode(
+                base64.b16decode(ssh_rsa.upper())), )
+
+        return ValueError("")
+
 
 class RSAImplementation(object):
     def __init__(self, **kwargs):
@@ -216,10 +250,13 @@ class RSAImplementation(object):
 
     def generate(self, bits, randfunc=None, progress_func=None):
         if bits < 1024 or (bits & 0xff) != 0:
-            # pubkey.getStrongPrime doesn't like anything that's not a multiple of 128 and > 512
-            raise ValueError("RSA modulus length must be a multiple of 256 and > 1024")
+            # pubkey.getStrongPrime doesn't like anything that's not a
+            # multiple of 128 and > 512
+            raise ValueError(
+                "RSA modulus length must be a multiple of 256 and > 1024")
         rf = self._get_randfunc(randfunc)
-        obj = _RSA.generate_py(bits, rf, progress_func)    # TODO: Don't use legacy _RSA module
+        # TODO: Don't use legacy _RSA module
+        obj = _RSA.generate_py(bits, rf, progress_func)    
         key = self._math.rsa_construct(obj.n, obj.e, obj.d, obj.p, obj.q, obj.u)
         return _RSAobj(self, key)
 
@@ -228,40 +265,39 @@ class RSAImplementation(object):
         return _RSAobj(self, key)
 
     def _importKeyDER(self, externKey):
-	der = DerSequence()
-	der.decode(externKey, True)
-	if len(der)==9 and der.hasOnlyInts() and der[0]==0:
-		# ASN.1 RSAPrivateKey element
-		del der[6:8]	# Remove d mod (p-1) and d mod (q-1)
-		del der[0]	# Remove version
-		return self.construct(der[:])
-	if len(der)==2:
-		# ASN.1 SubjectPublicKeyInfo element
-		if der[0]=='\x30\x0D\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x01\x01\x05\x00':
-			bitmap = DerObject()
-			bitmap.decode(der[1], True)
-			if bitmap.typeTag=='\x03' and bitmap.payload[0]=='\x00':
-				der.decode(bitmap.payload[1:], True)
-				if len(der)==2 and der.hasOnlyInts():
-					return self.construct(der[:])
-	raise ValueError("RSA key format is not supported")
+        der = DerSequence()
+        der.decode(externKey, True)
+        if len(der)==9 and der.hasOnlyInts() and der[0]==0:
+            # ASN.1 RSAPrivateKey element
+            del der[6:8]	# Remove d mod (p-1) and d mod (q-1)
+            del der[0]	# Remove version
+            return self.construct(der[:])
+        if len(der)==2:
+            # ASN.1 SubjectPublicKeyInfo element
+            if der[0]=='\x30\x0D\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x01\x01\x05\x00':
+                bitmap = DerObject()
+                bitmap.decode(der[1], True)
+                if bitmap.typeTag=='\x03' and bitmap.payload[0]=='\x00':
+                    der.decode(bitmap.payload[1:], True)
+                    if len(der)==2 and der.hasOnlyInts():
+                        return self.construct(der[:])
+        raise ValueError("RSA key format is not supported")
 
     def importKey(self, externKey):
-	"""Import an RSA key (public or private half).
-
-	externKey:	the RSA key to import, encoded as a string.
-			The key can be in DER (PKCS#1) or in unencrypted
-			PEM format (RFC1421).
-	"""
-	if externKey.startswith('-----'):
-		# This is probably a PEM encoded key
-		lines = externKey.replace(" ",'').split()
-		der = binascii.a2b_base64(''.join(lines[1:-1]))
-		return self._importKeyDER(der)
-	if externKey[0]=='\x30':
-		# This is probably a DER encoded key
-		return self._importKeyDER(externKey)
-	raise ValueError("RSA key format is not supported")
+        """Import an RSA key (public or private half).
+        externKey:	the RSA key to import, encoded as a string.
+        The key can be in DER (PKCS#1) or in unencrypted
+        PEM format (RFC1421).
+        """
+        if externKey.startswith('-----'):
+            # This is probably a PEM encoded key
+            lines = externKey.replace(" ", '').split()
+            der = binascii.a2b_base64(''.join(lines[1:-1]))
+            return self._importKeyDER(der)
+        if externKey[0]=='\x30':
+            # This is probably a DER encoded key
+            return self._importKeyDER(externKey)
+        raise ValueError("RSA key format is not supported")
 
 _impl = RSAImplementation()
 generate = _impl.generate
@@ -270,4 +306,3 @@ importKey = _impl.importKey
 error = _impl.error
 
 # vim:set ts=4 sw=4 sts=4 expandtab:
-
