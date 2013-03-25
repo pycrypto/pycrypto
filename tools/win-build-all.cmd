721e05d5326087589dcdb8886b2c10c8e4b5b55c
Dwayne Litzenberger <dlitz@dlitz.net>
2013-03-24 22:24:10 -0700

    Add tools/win-build-all.cmd

---
+++ tools/win-build-all.cmd
@@ -0,0 +1,22 @@
+:: This is my EXPERIMENTAL script for building on Windows.
+:: It might not work for you.  Sorry.
+:: -- dlitz
+
+C:\Python21\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python22\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python23\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python24\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python25\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python26\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python27\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python30\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python31\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python32\python       setup.py -q build -c mingw32 test bdist_wininst
+C:\Python33\python       setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python25-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python26-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python27-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python30-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python31-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python32-64\python setup.py -q build -c mingw32 test bdist_wininst
+:: C:\Python33-64\python setup.py -q build -c mingw32 test bdist_wininst
