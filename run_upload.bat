20b6e694d97218300d47067b7d675e49182238bd
Frederik Teichert <f@nerdig.org>
2016-08-10 15:20:35 +0200

    wheels can be built

---
+++ run_upload.bat
@@ -0,0 +1,31 @@
+echo "--------------------------------------------------------------------------------"
+echo "WINDOWS build"
+echo "--------------------------------------------------------------------------------"
+
+if "%ENABLE_UPLOAD%" == "false" (
+    echo "Skipping upload"
+    exit /b 0
+)
+
+@echo on
+
+echo Creating virtual env
+C:\Python27-amd64\Scripts\virtualenv -p %PYTHON_EXE% %VIRTENV_NAME_BUILD%
+echo Activating
+call %VIRTENV_NAME_BUILD%\Scripts\activate.bat
+
+@echo on
+
+echo "Python version"
+python -c "import sys; print(sys.version)"
+
+set PYTHONPATH=%WORKSPACE%
+
+# Build
+set -e
+
+set HOME=.
+rem python setup.py build_sphinx
+python setup.py bdist_wheel upload -r %INDEX_SERVER% || exit /b 1
+
+exit /b 0
---
+++ run_upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+echo "--------------------------------------------------------------------------------"
+echo "LINUX build"
+echo "--------------------------------------------------------------------------------"
+
+if [ ${ENABLE_UPLOAD} == "false" ]
+then
+    echo "Skipping upload"
+    exit 0
+fi
+
+virtualenv -p $PYTHON_EXE $VIRTENV_NAME_BUILD
+source $VIRTENV_NAME_BUILD/bin/activate
+
+echo "Python version"
+python -c "import sys; print(sys.version)"
+
+export PYTHONPATH=$WORKSPACE
+# Build
+set -e
+
+export HOME=.
+#python setup.py build_sphinx
+python setup.py bdist_wheel upload -r ${INDEX_SERVER}
+
+exit 0
